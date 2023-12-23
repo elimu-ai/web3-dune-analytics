3a2b2448e3245c6425e4ca7a18f829091ccf46a0
Bender Rodríguez <85234598+Bender-22@users.noreply.github.com>
2023-12-23 20:12:43 +0800

    Update balancer_v2_liquidity.sql

--- queries/dune-sql/balancer_v2_liquidity.sql
+++ queries/dune-sql/balancer_v2_liquidity.sql
@@ -2,7 +2,7 @@ WITH getBalance as (
 select day as date,
        pool_id as pool_address,
        CASE WHEN token_address = 0xe29797910D413281d2821D5d9a989262c8121CC2 THEN 'ELIMU' ELSE token_symbol END as symbol,
-       CASE WHEN token_address = 0xe29797910D413281d2821D5d9a989262c8121CC2 THEN pool_liquidity_eth END as balance
+       CASE WHEN token_address = 0xe29797910D413281d2821D5d9a989262c8121CC2 THEN token_balance END as balance
 from balancer_v2_ethereum.liquidity
 where pool_id IN (0x517390b2b806cb62f20ad340de6d98b2a8f17f2b0002000000000000000001ba)
 and day >= CAST('2021-07-01' AS TIMESTAMP)
